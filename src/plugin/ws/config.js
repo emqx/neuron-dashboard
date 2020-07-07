da392544ae4b54857c5a6dc2bfa4ae3f29882eac
ysfscream <894402575@qq.com>
2020-07-07 16:55:53 +0800

    feat: add env variable to config host

--- src/plugin/ws/config.js
+++ src/plugin/ws/config.js
@@ -1,8 +1,5 @@
-let customServerBaseUrl
-window.serverBaseUrl = 'ws://52.53.170.189:7000/'
-try {
-  customServerBaseUrl = window.serverBaseUrl
-} catch (e) {}
+console.log(process.env.VUE_APP_HOST)
+window.serverBaseUrl = process.env.VUE_APP_HOST
 
 const config = {
   isDevelopment: false,
@@ -12,21 +9,19 @@ const config = {
     CSRFTokenRequired: true,
     updateCSRFTokenIn: 5 * 60 * 1000 // ms
   },
-  //
   clientBaseUrl: '/',
-  // serverBaseUrl: customServerBaseUrl || '',
   clientLoginUrl: null
 }
 switch (process.env.NODE_ENV) {
   case 'production':
     Object.assign(config, {
-      serverBaseUrl: customServerBaseUrl || `ws://${window.location.hostname}:${window.location.port}/`
+      serverBaseUrl: window.serverBaseUrl || `ws://${window.location.hostname}:${window.location.port}/`
     })
     break
   case 'development':
     Object.assign(config, {
       isDevelopment: true,
-      serverBaseUrl: window.serverBaseUrl || 'ws://192.168.234.176:7681/'
+      serverBaseUrl: window.serverBaseUrl || 'ws://192.168.234.176:7000/'
     })
     break
 }
