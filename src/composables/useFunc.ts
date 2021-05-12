8354fd12a4fc725296e43a8e58974ecda1d01aa3
Kinplemelon <wujiu529@gmail.com>
2021-05-12 15:22:27 +0800

    feat(config): handle upload object table event

--- src/composables/useFunc.ts
+++ src/composables/useFunc.ts
@@ -2,16 +2,18 @@ import { readonly } from '@vue/reactivity'
 
 interface FuncModel {
   getObject: 46
+  setObject: 41
   searchObject: 52
   getDrivers: 23
   readLogs: 83
 }
 
-type FuncName = 'getObject' | 'searchObject' | 'getDrivers' | 'readLogs'
+type FuncName = keyof FuncModel
 
 export default function useFunc(funcName: FuncName): number {
   const funcMap: FuncModel = readonly({
     getObject: 46,
+    setObject: 41,
     searchObject: 52,
     getDrivers: 23,
     readLogs: 83,
