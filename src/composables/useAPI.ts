7f86ec4cb4b9b0e578ccbd1a129ba9af6231408b
oceanlvr <657531018@qq.com>
2021-05-14 16:37:50 +0800

    fix(driverSetup): fix driver setup filed fail issue

--- src/composables/useAPI.ts
+++ src/composables/useAPI.ts
@@ -2,6 +2,7 @@ import useWebsocket from '@/plugins/ws/useWebsocket'
 import { useStore } from 'vuex'
 import useFunc from '@/composables/useFunc'
 import { ObjdModel, ChnlModel, NeuronData } from '@/types/neuron'
+import { EmqxMessage } from '@emqx/emqx-ui'
 
 export default function useAPI(): {
   addObjectData: (data: Array<Omit<ObjdModel, 'func'>>) => void
@@ -9,22 +10,24 @@ export default function useAPI(): {
   setGatewayRestartNew: () => void
 } {
   const store = useStore()
-  const ws = useWebsocket().ws()
+  const { ws } = useWebsocket()
 
   const addObjectData = (data: Array<Omit<ObjdModel, 'func'>>) => {
     const { objd } = store.state
-    ws.send({ func: useFunc('setObject'), objd: objd.concat(data) })
+    ws().send({ func: useFunc('setObject'), objd: objd.concat(data) })
   }
 
   const setDriverData = (data?: Array<Omit<ChnlModel, 'func'>>) => {
     const { _chnl } = store.state
     const chnl = data ? data : _chnl
-    ws.set({ success: afterSetDriverData }).send({ func: useFunc('setChannel'), chnl })
+    ws()
+      .set({ success: afterSetDriverData })
+      .send({ func: useFunc('setChannel'), chnl })
   }
 
   //Function 70. After set DriverData, we must to restart gateway
   const setGatewayRestartNew = () => {
-    ws.send({ func: useFunc('gatewayRestartNew'), acts: 'restartnew' })
+    ws().send({ func: useFunc('gatewayRestartNew'), acts: 'restartnew' })
   }
 
   return {
@@ -35,8 +38,12 @@ export default function useAPI(): {
 }
 
 const afterSetDriverData = (data: NeuronData) => {
-  const { setGatewayRestartNew } = useAPI()
   if (data.errc === 0) {
-    setGatewayRestartNew()
+    console.log(data)
+    const { setGatewayRestartNew } = useAPI()
+    setTimeout(() => {
+      setGatewayRestartNew()
+    }, 2000)
+    EmqxMessage.success()
   }
 }
