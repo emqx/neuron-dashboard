b2db70c06399dff7baa74ab03126a8e86940444c
Kinplemelon <wujiu529@gmail.com>
2021-05-17 19:42:16 +0800

    feat(global): add file about configured constants

---
+++ src/config/index.ts
@@ -0,0 +1,136 @@
+export const AttributeTypeList = [
+  {
+    val: 'word',
+  },
+  {
+    val: 'uword',
+  },
+  {
+    val: 'dword',
+  },
+  {
+    val: 'udword',
+  },
+  {
+    val: 'float',
+  },
+  {
+    val: 'double',
+  },
+  {
+    val: 'bit',
+  },
+]
+
+export const Operator = [
+  {
+    val: '>',
+  },
+  {
+    val: '>=',
+  },
+  {
+    val: '==',
+  },
+  {
+    val: '<',
+  },
+  {
+    val: '<=',
+  },
+  {
+    val: '!=',
+  },
+  {
+    val: '&',
+  },
+  {
+    val: '^',
+  },
+  {
+    val: '|',
+  },
+]
+
+export const EventCategory = [
+  {
+    val: 'critical',
+  },
+  {
+    val: 'alarm',
+  },
+  {
+    val: 'warning',
+  },
+  {
+    val: 'event',
+  },
+  {
+    val: 'view',
+  },
+]
+
+export const ScriptList = [
+  {
+    val: 'COMMENT',
+  },
+  {
+    val: 'DECLARE',
+  },
+  {
+    val: 'INIT',
+  },
+  {
+    val: 'DO',
+  },
+  {
+    val: 'IF',
+  },
+  {
+    val: 'THEN',
+  },
+  {
+    val: 'ELSE IF',
+  },
+  {
+    val: 'ELSE',
+  },
+  {
+    val: 'POS',
+  },
+]
+
+export const UserLevels = [
+  {
+    label: 'VIEW(0)',
+    key: 0,
+  },
+  {
+    label: 'OPERATOR(1)',
+    key: 1,
+  },
+  {
+    label: 'FOREMAN(2)',
+    key: 2,
+  },
+  {
+    label: 'MAINENANCE(3)',
+    key: 3,
+  },
+  {
+    label: 'SUPERVISOR(4)',
+    key: 4,
+  },
+  {
+    label: 'ENGINEER(5)',
+    key: 5,
+  },
+  {
+    label: 'DESIGNER(6)',
+    key: 6,
+  },
+  {
+    label: 'MANAGER(7)',
+    key: 7,
+  },
+]
