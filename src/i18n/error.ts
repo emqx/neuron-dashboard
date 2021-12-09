9e76bfc934ff2a104b21b776570c252ed465ac2c
Kinplemelon <wujiu529@gmail.com>
2021-12-09 14:17:57 +0800

    refactor(http): update error code & add chinese

---
+++ src/i18n/error.ts
@@ -0,0 +1,106 @@
+export default {
+  [1001]: {
+    zh: '内部错误',
+    en: 'Internal error',
+  },
+  [1002]: {
+    zh: '请求正文无效',
+    en: 'Request body invalid',
+  },
+  [1003]: {
+    zh: '请求参数无效',
+    en: 'Request param invalid',
+  },
+  [2000]: {
+    zh: '未找到插件名称',
+    en: 'Plugin name not found',
+  },
+  [2001]: {
+    zh: '节点类型无效',
+    en: 'Node type invalid',
+  },
+  [2002]: {
+    zh: '节点已存在',
+    en: 'Node exist',
+  },
+  [2003]: {
+    zh: '节点不存在',
+    en: 'Node not exist',
+  },
+  [2004]: {
+    zh: 'Group 配置不存在',
+    en: 'Group config not exist',
+  },
+  [2005]: {
+    zh: 'Tag 不存在',
+    en: 'Tag not exist',
+  },
+  [2006]: {
+    zh: '不支持 Tag 属性',
+    en: 'Tag attribute not support',
+  },
+  [2007]: {
+    zh: '节点设置无效',
+    en: 'Node setting invalid',
+  },
+  [2008]: {
+    zh: '节点设置不存在',
+    en: 'Node setting not exist',
+  },
+  [2009]: {
+    zh: '正在使用的 Group 配置',
+    en: 'Group config in use',
+  },
+  [2010]: {
+    zh: '节点未就绪',
+    en: 'Node not ready',
+  },
+  [2011]: {
+    zh: '节点正在运行',
+    en: 'Node is running',
+  },
+  [2012]: {
+    zh: '节点未运行',
+    en: 'Node not running',
+  },
+  [2013]: {
+    zh: '节点已停止',
+    en: 'Node is stopped',
+  },
+  [2014]: {
+    zh: 'Group 未订阅',
+    en: 'Group not subscribe',
+  },
+  [2015]: {
+    zh: '不支持 Tag 类型',
+    en: 'Tag type not support',
+  },
+  [2016]: {
+    zh: 'Tag 配置已存在',
+    en: 'Group config exist',
+  },
+  [3000]: {
+    zh: '设备连接失败',
+    en: 'Device connection failure',
+  },
+  [3001]: {
+    zh: '设备读取失败',
+    en: 'Device read failure',
+  },
+  [3002]: {
+    zh: '设备写入失败',
+    en: 'Device write failure',
+  },
+  [3003]: {
+    zh: '设备异常',
+    en: 'Device abnormality',
+  },
+  [3004]: {
+    zh: '设备 Tag 不存在',
+    en: 'Device tag not exist',
+  },
+  [3005]: {
+    zh: '设备 Tag 类型不匹配',
+    en: 'Device tag type not match',
+  },
+}
