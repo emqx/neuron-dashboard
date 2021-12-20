44d8ebbce1a1a51a3ead76633c71121c24d9d4fa
Kinplemelon <wujiu529@gmail.com>
2021-12-20 10:53:30 +0800

    feat(config): add 'filterable' to some select

--- src/views/monitoring/DataMonitoring.vue
+++ src/views/monitoring/DataMonitoring.vue
@@ -11,13 +11,13 @@
       <div class="target-select">
         <div>
           <label>{{ $t('config.southDevice') }}</label>
-          <emqx-select v-model="currentGroup.nodeID" @change="selectedNodeChanged">
+          <emqx-select v-model="currentGroup.nodeID" @change="selectedNodeChanged" filterable>
             <emqx-option v-for="item in nodeList" :key="item.id" :value="item.id" :label="item.name" />
           </emqx-select>
         </div>
         <div>
           <label>{{ $t('config.groupName') }}</label>
-          <emqx-select v-model="currentGroup.groupName" @change="selectedGroupChanged">
+          <emqx-select v-model="currentGroup.groupName" @change="selectedGroupChanged" filterable>
             <emqx-option v-for="item in groupList" :key="item.name" :value="item.name" :label="item.name" />
           </emqx-select>
         </div>
