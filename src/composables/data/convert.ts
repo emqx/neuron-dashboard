c5882fc91d169b24ddaa9033c64116f94d8c759c
Kinplemelon <wujiu529@gmail.com>
2022-02-11 18:31:00 +0800

    feat(convert.js): some simple comments

--- src/composables/data/convert.ts
+++ src/composables/data/convert.ts
@@ -1,5 +1,4 @@
 import { TagType } from '@/types/enums'
-import {} from 'lodash'
 
 const floatMsgMap = {
   [TagType.FLOAT]: {
@@ -22,6 +21,9 @@ const uintLengthMap = {
   [TagType.UINT64]: 64,
 }
 
+/**
+ * '101110111011' -> ['1011', '1011', '1011']
+ */
 const chunkStrFromEnd = (str: string, chunkSize: number) => {
   const ret = []
   for (let index = str.length; index > 0; index -= chunkSize) {
@@ -31,10 +33,16 @@ const chunkStrFromEnd = (str: string, chunkSize: number) => {
   return ret.reverse()
 }
 
+/**
+ * aabbccdd => aa bb cc dd
+ */
 const insertString = (str: string, joiner: string, splitLength = 2) => {
   return chunkStrFromEnd(str, splitLength).join(joiner)
 }
 
+/**
+ * count: 01 + 10 = 11
+ */
 const addBinary = (str1: string, str2: string) => {
   const arrForRet = []
   let binaryStr1 = str1
@@ -68,6 +76,9 @@ const addBinary = (str1: string, str2: string) => {
   return arrForRet.join('')
 }
 
+/**
+ * 101 -> 0101 or 101 -> 1010
+ */
 const fillString = (str: string, filler: string, length: number, isFillFront: boolean) => {
   if (str == '' || filler.length != 1 || length <= str.length) {
     return str
@@ -88,6 +99,10 @@ const getSignBit = (isNegative: boolean) => (isNegative ? 1 : 0)
 
 const checkBinaryStr = (str: string) => /^(0|1)+$/.test(str)
 
+/**
+ * https://en.wikipedia.org/wiki/Two%27s_complement
+ * 0101 -> 1010
+ */
 const getComplement = (str: string) => {
   if (!checkBinaryStr(str)) {
     return
@@ -97,6 +112,17 @@ const getComplement = (str: string) => {
     .join('')
 }
 
+/**
+ * because toString(2) has the problem of loss of precision when the number is too large
+ */
+const transHexStrToBinaryStr = (str: string) =>
+  Array.from(str)
+    .map((item) => fillString(parseInt(item, 16).toString(2), '0', 4, true))
+    .join('')
+
+/**
+ * '1010' => 'a5'
+ */
 const transA4DigitBinaryNumberStrToHexStr = (str: string) => {
   if (!checkBinaryStr(str)) {
     return
@@ -112,14 +138,9 @@ const transA4DigitBinaryNumberStrToHexStr = (str: string) => {
 }
 
 /**
- * because toString(2) has the problem of loss of precision when the number is too large
+ * 1010 0101 -> a5
  */
-const transHexStrToBinaryStr = (str: string) =>
-  Array.from(str)
-    .map((item) => fillString(parseInt(item, 16).toString(2), '0', 4, true))
-    .join('')
-
-const transDecimalStrToHexStr = (str: string) => {
+const transBinaryStrToHexStr = (str: string) => {
   if (!checkBinaryStr(str)) {
     return str
   }
@@ -131,6 +152,10 @@ const transDecimalStrToHexStr = (str: string) => {
   return strChunkArr.map((binaryBlock) => transA4DigitBinaryNumberStrToHexStr(binaryBlock)).join('')
 }
 
+/**
+ * like this: https://gregstoll.com/~gregstoll/floattohex/
+ * Use binary as an intermediary
+ */
 export const transFloatNumberToHex = (num: number, type: TagType.FLOAT | TagType.DOUBLE): string => {
   if (Number.isNaN(num)) {
     return 'Error'
@@ -180,11 +205,14 @@ export const transFloatNumberToHex = (num: number, type: TagType.FLOAT | TagType
   mantissaPart = mantissaPart.substring(1)
   exponentPart = (exponentBaseNum + numForAddToExponent).toString(2)
   exponentPart = fillString(exponentPart, '0', exponentLength, true)
-  let rawStr = transDecimalStrToHexStr(`${flagBit}${exponentPart}${mantissaPart}`)
+  let rawStr = transBinaryStrToHexStr(`${flagBit}${exponentPart}${mantissaPart}`)
   rawStr = fillString(rawStr, '0', exponentLength, true)
   return insertString(rawStr, ' ', 2)
 }
 
+/**
+ * desc: http://blog.sina.com.cn/s/blog_a4acf5ea0102z2r7.html
+ */
 export const transNegativeNumberToHex = (
   num: number,
   type: TagType.UINT8 | TagType.UINT16 | TagType.UINT32 | TagType.UINT64,
@@ -199,9 +227,12 @@ export const transNegativeNumberToHex = (
   if (decimalNumComplementStrAdd1.length > uintLengthMap[type] - 1) {
     return num
   }
-  return insertString(transDecimalStrToHexStr(getSignBit(true) + decimalNumComplementStrAdd1), ' ')
+  return insertString(transBinaryStrToHexStr(getSignBit(true) + decimalNumComplementStrAdd1), ' ')
 }
 
+/**
+ * common base conversion
+ */
 export const transPositiveIntegerToHex = (value: string) => {
   return Number(value).toString(16)
 }
@@ -269,6 +300,7 @@ export const transFloatHexToDecimalNum = (hexStr: string, type: TagType.FLOAT | 
 
 /**
  * For uint
+ * desc: http://blog.sina.com.cn/s/blog_a4acf5ea0102z2r7.html reverse process
  */
 export const transUintHexToDecimalNum = (
   unitHex: string,
