148744d7be0566487b8d14e355035d05f855c098
Kinplemelon <wujiu529@gmail.com>
2022-02-11 14:04:32 +0800

    refactor(error): update error codes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,11 @@ export const HEXADECIMAL_PREFIX = '0x'
 
 export const MAX_NUM_IN_A_ROW_ON_THE_OVERVIEW = 3
 
+/**
+ * When updating here, the @/i18n/error.ts file should be updated together.
+ * doc: https://github.com/neugates/nep/blob/main/docs/Neuron%202.x%20ERROR%20CODE.md
+ */
 export const ERROR_CODE_ARR = [
-  1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-  2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 3000, 3001, 3002, 3003, 3004, 3005,
+  1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2101,
+  2102, 2103, 2201, 2202, 2203, 2204, 2205, 2301, 2302, 2303, 3000, 3001, 3002, 3003, 3004, 3005, 3006,
 ]
