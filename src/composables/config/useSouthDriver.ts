3bb7dc4284702ec5e8d16e766fd9468b4778c342
Kinplemelon <wujiu529@gmail.com>
2022-04-19 13:46:19 +0800

    feat(config): support timing refresh status

--- src/composables/config/useSouthDriver.ts
+++ src/composables/config/useSouthDriver.ts
@@ -1,15 +1,17 @@
 import { querySouthDriverList } from '@/api/config'
 import { DriverItemInList } from '@/types/config'
-import { ref, Ref } from 'vue'
+import { onUnmounted, ref, Ref } from 'vue'
 import { useFillNodeListStatusData } from './useNodeList'
 import { useGetPluginMsgIdMap } from './usePlugin'
 
-export default (autoLoad = true) => {
-  const fillNodeListStatusData = useFillNodeListStatusData()
+export default (autoLoad = true, needRefreshStatus = false) => {
+  const { fillNodeListStatusData } = useFillNodeListStatusData()
 
   const southDriverList: Ref<Array<DriverItemInList>> = ref([])
   const isListLoading: Ref<boolean> = ref(false)
   const { pluginMsgIdMap, initMsgIdMap } = useGetPluginMsgIdMap()
+
+  let refreshStatusTimer: undefined | number = undefined
 
   const getSouthDriverList = async () => {
     isListLoading.value = true
@@ -25,9 +27,31 @@ export default (autoLoad = true) => {
     isListLoading.value = false
   }
 
+  const startTimer = () => {
+    refreshStatusTimer = window.setInterval(async () => {
+      const driverListAddStatus = await fillNodeListStatusData(southDriverList.value)
+      southDriverList.value = driverListAddStatus.map((item) => {
+        return {
+          ...item,
+          plugin: pluginMsgIdMap[item.plugin_id].name,
+        }
+      })
+    }, 15 * 1000)
+  }
+
   if (autoLoad) {
     getSouthDriverList()
   }
+
+  if (needRefreshStatus) {
+    startTimer()
+  }
+
+  onUnmounted(() => {
+    if (refreshStatusTimer) {
+      window.clearInterval(refreshStatusTimer)
+    }
+  })
 
   return {
     southDriverList,
