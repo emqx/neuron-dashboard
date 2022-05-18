cdeed411ec589f7cf805cdbb84283481b51e0afb
Kinplemelon <wujiu529@gmail.com>
2022-05-18 16:58:24 +0800

    chore(global): init qiankun and add ekuiper

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,30 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from './routes'
 import store from '@/store/index'
+import { loadMicroApp } from 'qiankun'
 
 const router = createRouter({
   history: createWebHashHistory(process.env.BASE_URL),
   routes,
 })
 
+const handleEKuiper = async () => {
+  await new Promise((resolve) => window.setTimeout(resolve, 100))
+  loadMicroApp({
+    name: 'ekuiper',
+    entry: '//localhost:3002',
+    container: '#page-content',
+  })
+}
+
 router.beforeEach((to, from, next) => {
   if (!store.state.token && to.name !== 'Login') {
     next({ name: 'Login' })
   } else if (store.state.token && to.name === 'Login') {
     next('/')
+  } else if (/^\/ekuiper/.test(to.path)) {
+    next()
+    handleEKuiper()
   } else {
     next()
   }
