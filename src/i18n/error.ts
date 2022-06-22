6a6dfcf2dd92372965a40fb32bca5c9fdc02e71d
Kinplemelon <wujiu529@gmail.com>
2022-06-22 15:16:58 +0800

    refactor(error): upgrade error code

--- src/i18n/error.ts
+++ src/i18n/error.ts
@@ -2,16 +2,20 @@
  * When updating here, the @/utils/constants.ts file ERROR_CODE_ARR should be updated together.
  */
 export default {
+  [1000]: {
+    zh: '通用错误',
+    en: 'Generic error',
+  },
   [1001]: {
     zh: '内部错误',
     en: 'Internal error',
   },
   [1002]: {
-    zh: '请求正文无效',
+    zh: '请求 body 无效',
     en: 'Request body invalid',
   },
   [1003]: {
-    zh: '请求参数无效',
+    zh: '请求 param 无效',
     en: 'Request param invalid',
   },
   [1004]: {
@@ -23,7 +27,7 @@ export default {
     en: 'Decoding token error',
   },
   [1006]: {
-    zh: '过期令牌',
+    zh: '令牌过期',
     en: 'Expired token',
   },
   [1007]: {
@@ -31,92 +35,116 @@ export default {
     en: 'Validate token error',
   },
   [1008]: {
-    zh: '令牌无效',
+    zh: '无效令牌',
     en: 'Invalid token',
   },
   [1009]: {
-    zh: '用户或密码错误',
+    zh: '用户名或密码错误',
     en: 'User or password error',
   },
-  [2001]: {
-    zh: '节点类型无效',
-    en: 'Node type invalid',
+  [1010]: {
+    zh: '程序繁忙',
+    en: 'Is busy',
   },
   [2002]: {
-    zh: '节点存在',
+    zh: 'node 已存在',
     en: 'Node exist',
   },
   [2003]: {
-    zh: '节点不存在',
+    zh: 'node 不存在',
     en: 'Node not exist',
   },
   [2004]: {
-    zh: '节点设置无效',
+    zh: 'node 设置无效',
     en: 'Node setting invalid',
   },
   [2005]: {
-    zh: '未找到节点设置',
+    zh: 'node 设置未找到',
     en: 'Node setting not found',
   },
   [2006]: {
-    zh: '节点未准备好',
+    zh: 'node 未准备好',
     en: 'Node not ready',
   },
   [2007]: {
-    zh: '节点正在运行',
+    zh: 'node 正在运行',
     en: 'Node is running',
   },
   [2008]: {
-    zh: '节点未运行',
+    zh: 'node 未运行',
     en: 'Node not running',
   },
   [2009]: {
-    zh: '节点停止',
+    zh: 'node 已停止',
     en: 'Node is stopped',
   },
   [2101]: {
-    zh: '组配置不存在',
-    en: 'Group config not exist',
+    zh: 'group 已经被订阅',
+    en: 'Group already subscribed',
   },
   [2102]: {
-    zh: '正在使用的组配置',
-    en: 'Group config in use',
+    zh: 'group 未被订阅',
+    en: 'Group not subscribe',
   },
   [2103]: {
-    zh: '组配置冲突',
-    en: 'Group config conflict',
+    zh: 'group 不允许',
+    en: 'Group not allow',
+  },
+  [2104]: {
+    zh: 'group 已存在',
+    en: 'Group exist',
+  },
+  [2105]: {
+    zh: 'group 参数无效',
+    en: 'Group parameter invalid',
+  },
+  [2106]: {
+    zh: 'group 不存在',
+    en: 'Group not exist',
   },
   [2201]: {
-    zh: '标签不存在',
+    zh: 'tag 不存在',
     en: 'Tag not exist',
   },
   [2202]: {
-    zh: '标签名称冲突',
+    zh: 'tag 名称冲突',
     en: 'Tag name conflict',
   },
   [2203]: {
-    zh: '标签属性不支持',
+    zh: 'tag 属性不支持',
     en: 'Tag attribute not support',
   },
   [2204]: {
-    zh: '标签类型不支持',
+    zh: 'tag 类型不支持',
     en: 'Tag type not support',
   },
   [2205]: {
-    zh: '标签地址格式无效',
+    zh: 'tag 地址格式无效',
     en: 'Tag address format invalid',
   },
   [2301]: {
-    zh: '找不到图书馆',
+    zh: '库未找到',
     en: 'Library not found',
   },
   [2302]: {
-    zh: '图书馆信息无效',
+    zh: '库信息无效',
     en: 'Library info invalid',
   },
   [2303]: {
-    zh: '库名冲突',
+    zh: '库名称冲突',
     en: 'Library name conflict',
+  },
+  [2304]: {
+    zh: '库打开失败',
+    en: 'Library failed to open',
+  },
+  [2305]: {
+    zh: '库模块无效',
+    en: 'Libraray module invalid',
+  },
+  [2306]: {
+    zh: '系统库不允许删除',
+    en: 'Library system not allow del',
   },
   [2400]: {
     zh: 'license 未找到',
@@ -143,35 +171,27 @@ export default {
     en: 'Reach licensed max number of tags per node',
   },
   [3000]: {
-    zh: '插件读取失败',
+    zh: '插件读失败',
     en: 'Plugin read failure',
   },
   [3001]: {
-    zh: '插件写入失败',
+    zh: '插件写失败',
     en: 'Plugin write failure',
   },
   [3002]: {
-    zh: '插件断开连接',
+    zh: '插件未连接',
     en: 'Plugin disconnected',
   },
   [3003]: {
-    zh: '插件标签不允许读取',
+    zh: '插件 tag 不允许读',
     en: 'Plugin tag not allow read',
   },
   [3004]: {
-    zh: '插件标签不允许写入',
+    zh: '插件 tag 不允许写',
     en: 'Plugin tag not allow write',
   },
-  [3005]: {
-    zh: '插件标签不存在',
-    en: 'Plugin tag not exist',
-  },
-  [3006]: {
-    zh: '插件组配置未订阅',
-    en: 'Plugin group config not subscribe',
-  },
   [3007]: {
-    zh: '插件标签类型不匹配',
+    zh: '插件 tag 类型不匹配',
     en: 'Plugin tag type mismatch',
   },
 }
