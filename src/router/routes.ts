8c58e3bea75e135ff76cbc21c68e3e221d1a2b14
orange <gezuxia@163.com>
2022-07-13 22:40:20 +0800

    perf(breadcurmb、router): add global breadcrumb and modified router

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,6 +2,7 @@ import type { RouteRecordRaw } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import { DriverDirection } from '@/types/enums'
+// import { propsToAttrMap } from '@vue/shared'
 
 export const LOGIN_ROUTE_NAME = 'Login'
 
@@ -10,101 +11,182 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     redirect: '/configuration/north-driver',
+  },
+  /** Overview */
+  // {
+  //   path: '/overview',
+  //   name: 'Overview',
+  //   meta: { title: 'common.overview' },
+  //   component: Home,
+  //   children: [
+  //     {
+  //       path: '/overview',
+  //       name: 'Overview',
+  //       meta: { requireAuth: true },
+  //       component: () => import('@/views/overview/Index.vue'),
+  //     },
+  //   ],
+  // },
+  /* MONITORING */
+  {
+    path: '/monitoring',
+    name: 'Monitoring',
+    meta: { title: 'data.monitoring' },
     component: Home,
     children: [
-      // {
-      //   path: '/overview',
-      //   name: 'Overview',
-      //   meta: { requireAuth: true },
-      //   component: () => import('@/views/overview/Index.vue'),
-      // },
-      /* MONITORING */
       {
-        path: '/monitoring/data',
+        path: 'data',
         name: 'DataMonitoring',
         component: () => import('@/views/monitoring/DataMonitoring.vue'),
+        meta: { hiddenBreadcrumb: true },
       },
-      /* CONFIG */
+    ],
+  },
+  /* CONFIG- NorthAPP */
+  {
+    path: '/configuration/north-driver',
+    name: 'NorthAPP',
+    meta: { title: 'config.northAppSetup' },
+    component: Home,
+    children: [
       {
-        path: '/configuration/north-driver',
+        path: '',
         name: 'NorthDriver',
         component: () => import('@/views/config/northDriver/Index.vue'),
+        meta: { hiddenBreadcrumb: true },
       },
       {
-        path: '/configuration/north-driver/:node',
+        path: ':node',
         name: 'NorthDriverGroup',
         component: () => import('@/views/config/northDriver/Group.vue'),
+        meta: { title: 'config.groupList' },
       },
       {
-        path: '/configuration/north-driver/config/:node',
+        path: 'config/:node',
         name: 'NorthDriverConfig',
         component: () => import('@/views/config/NodeConfig.vue'),
         props: {
           direction: DriverDirection.North,
         },
+        meta: { title: 'config.appConfig' },
       },
+    ],
+  },
+  /* CONFIG- SouthDevice */
+  {
+    path: '/configuration/south-driver',
+    name: 'SouthDevice',
+    meta: { title: 'config.southDeviceManagement' },
+    component: Home,
+    children: [
       {
-        path: '/configuration/south-driver',
+        path: '',
         name: 'SouthDriver',
         component: () => import('@/views/config/southDriver/Index.vue'),
+        meta: { hiddenBreadcrumb: true },
       },
       {
-        path: '/configuration/south-driver/:node',
-        name: 'SouthDriverGroup',
-        component: () => import('@/views/config/southDriver/Group.vue'),
-      },
-      {
-        path: '/configuration/south-driver/config/:node',
+        path: 'config/:node',
         name: 'SouthDriverConfig',
         component: () => import('@/views/config/NodeConfig.vue'),
         props: {
           direction: DriverDirection.South,
         },
+        meta: { title: 'config.deviceConfig' },
       },
       {
-        path: '/configuration/south-driver/:node/:group',
-        name: 'SouthDriverGroupTag',
-        component: () => import('@/views/config/southDriver/Tag.vue'),
+        path: ':node',
+        name: 'SouthDriverGroupG',
+        component: () => import('@/components/LayoutContent.vue'),
+        meta: { title: 'config.groupList' },
+        children: [
+          {
+            path: '',
+            name: 'SouthDriverGroup',
+            component: () => import('@/views/config/southDriver/Group.vue'),
+          },
+          {
+            path: ':group',
+            name: 'SouthDriverGroupTag',
+            component: () => import('@/views/config/southDriver/Tag.vue'),
+            meta: { title: 'config.tagList' },
+          },
+          {
+            path: ':group/add',
+            name: 'SouthDriverGroupAddTag',
+            component: () => import('@/views/config/southDriver/AddTag.vue'),
+            meta: { title: 'config.addTags' },
+          },
+        ],
       },
+    ],
+  },
+  /** Plugin */
+  {
+    path: '/configuration/plugin',
+    name: 'Plugin',
+    meta: { title: 'config.pluginManagement' },
+    component: Home,
+    children: [
       {
-        path: '/configuration/south-driver/:node/:group/add',
-        name: 'SouthDriverGroupAddTag',
-        component: () => import('@/views/config/southDriver/AddTag.vue'),
-      },
-      {
-        path: '/configuration/plugin',
+        path: '',
         name: 'PluginManagement',
         component: () => import('@/views/config/plugin/Index.vue'),
+        meta: { hiddenBreadcrumb: true },
       },
-      /* ADMIN */
+    ],
+  },
+  /** ADMIN */
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Home,
+    children: [
       {
-        path: '/admin/account-settings',
+        path: 'account-settings',
         name: 'AccountSettings',
-        meta: { requireAuth: true },
+        meta: { title: 'common.accountSettings', requireAuth: true, hiddenBreadcrumb: true },
         component: () => import('@/views/admin/AccountSetting.vue'),
       },
       {
-        path: '/admin/log',
-        name: 'Log',
-        meta: { requireAuth: true },
+        path: 'log',
+        name: 'AdminLog',
+        meta: { title: 'admin.log', requireAuth: true, hiddenBreadcrumb: true },
         component: () => import('@/views/admin/Log.vue'),
       },
-      /* License */
+    ],
+  },
+  /* License */
+  {
+    path: '/license',
+    name: 'License',
+    meta: { title: 'License' },
+    component: Home,
+    children: [
       {
         path: '/license',
         name: 'License',
         meta: { requireAuth: true },
         component: () => import('@/views/about/License.vue'),
       },
-      /* About */
+    ],
+  },
+  /* About */
+  {
+    path: '/about',
+    name: 'About',
+    meta: { title: 'common.about' },
+    component: Home,
+    children: [
       {
-        path: '/about',
+        path: '',
         name: 'About',
         meta: { requireAuth: true },
         component: () => import('@/views/about/About.vue'),
       },
     ],
   },
+
   {
     path: '/:ekuiper(.*)*',
     component: Home,
