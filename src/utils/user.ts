080c72941df1b82790d4af73723a0dcbf2e1aad2
orange <gezuxia@163.com>
2022-09-02 16:36:06 +0800

    fixed(breadcrumb): Support nested routing with parameters

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -1,4 +1,5 @@
 export const LOCAL_STORAGE_TOKEN_KEY = 'token'
+export const LOCAL_STORAGE_BREADCRUMB = 'breadcrumbs'
 
 export const getToken = (): string | null => window.localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY)
 
@@ -9,4 +10,13 @@ export const setToken = (token: string): void => {
 export const clearToken = (): void => {
   window.localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY)
 }
+
 export const clearLocalStorage = () => localStorage.clear()
+
+export const setBreadcrumbFullPaths = (breadcrumbs: string): void => {
+  window.localStorage.setItem(LOCAL_STORAGE_BREADCRUMB, breadcrumbs)
+}
+export const getBreadcrumbFullPaths = (): string => {
+  const res: string = window.localStorage.getItem(LOCAL_STORAGE_BREADCRUMB) || ''
+  return res
+}
