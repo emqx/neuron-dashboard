c57a07e246ff16ae4e4cdede4af94b1882d2fa35
orange <gezuxia@163.com>
2022-11-03 18:15:31 +0800

    feat(publicPath): modify publicPath to /web/ (also modify ekuiper publicPath to /web/ekuiper), and change port to 7000 instead of 70001(ekuiper is also)

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -92,7 +92,8 @@ export const countBaseURL = () => {
     return '/api/v2'
   }
   const { hostname, port, protocol, origin } = window.location
-  const serverURL = port ? `${protocol}//${hostname}:${(Number(port) + 1).toString()}` : origin
+  // const serverURL = port ? `${protocol}//${hostname}:${(Number(port) + 1).toString()}` : origin
+  const serverURL = port ? `${protocol}//${hostname}:${Number(port).toString()}` : origin
   return `${serverURL}/api/v2`
 }
 
