6454334360c209d37aa798b62243eea9a4872d12
orange <gezuxia@163.com>
2023-01-05 18:28:18 +0800

    refactor(api): modify schema api param plugin name to schema name

--- src/composables/config/useAddTag.ts
+++ src/composables/config/useAddTag.ts
@@ -9,6 +9,7 @@ import { useI18n } from 'vue-i18n'
 import type TagFormCom from '@/views/config/southDriver/components/TagForm.vue'
 import { createRandomString, getErrorMsg, jumpToFirstErrorFormItem, popUpErrorMessage } from '@/utils/utils'
 import { useNodeMsgMap } from './useNodeList'
+import { useGetPluginMsgIdMap } from './usePlugin'
 
 export const useTagTypeSelect = () => {
   const tagTypeOptList = Object.keys(TagType)
@@ -151,12 +152,20 @@ export default () => {
   // Limit the type of tag
   const nodePluginInfo: Ref<PluginInfo> = ref({} as PluginInfo)
 
+  const { pluginMsgIdMap, initMsgIdMap } = useGetPluginMsgIdMap()
+
   const getNodePluginInfo = async () => {
     await initMap()
-    const { data } = await queryPluginConfigInfo(getNodeMsgById(node.value).plugin)
-    const plugin: PluginInfo = data
-    if (plugin) {
-      nodePluginInfo.value = plugin
+    await initMsgIdMap()
+    const pluginName = getNodeMsgById(node.value).plugin
+    const schemaName = pluginMsgIdMap[pluginName]?.schema
+
+    if (schemaName) {
+      const { data } = await queryPluginConfigInfo(schemaName)
+      const plugin: PluginInfo = data
+      if (plugin) {
+        nodePluginInfo.value = plugin
+      }
     }
   }
 
