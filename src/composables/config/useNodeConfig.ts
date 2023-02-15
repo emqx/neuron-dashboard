9e200a4fbd5a84855867f5cf9da2c03086c5dcd6
zuxia.ge <zuxia.ge@emqx.io>
2023-02-15 15:58:24 +0800

    fixed(schema): fields isnot change when swith a filed, such as switch link field

--- src/composables/config/useNodeConfig.ts
+++ src/composables/config/useNodeConfig.ts
@@ -149,7 +149,7 @@ export default (props: Props) => {
       const regexExpression = new RegExp(regex)
       return regexExpression.test(fieldValue)
     }
-    return value ? fieldValue === value : true
+    return value !== undefined || value !== null ? fieldValue === value : true
   }
 
   const cancel = () => {
