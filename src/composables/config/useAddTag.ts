90ae25582ce111b5f319c2330d85e31ac028afeb
zuxia.ge <zuxia.ge@emqx.io>
2023-05-22 12:30:20 +0800

    refactor(node tag): add error message, when the tag list form validate is error

--- src/composables/config/useAddTag.ts
+++ src/composables/config/useAddTag.ts
@@ -1,5 +1,5 @@
 import { addTag, queryPluginConfigInfo } from '@/api/config'
-import type { PluginInfo, TagFormItem, AddTagListForm } from '@/types/config'
+import type { PluginInfo, TagFormItem, AddTagListForm, TagForm } from '@/types/config'
 import { DriverDirection, TagAttributeType, TagType } from '@/types/enums'
 import type { Ref } from 'vue'
 import { ref, computed } from 'vue'
@@ -7,7 +7,7 @@ import { useRoute, useRouter } from 'vue-router'
 import { EmqxMessage } from '@emqx/emqx-ui'
 import { useI18n } from 'vue-i18n'
 import type TagFormCom from '@/views/config/southDriver/components/TagForm.vue'
-import { createRandomString, getErrorMsg, popUpErrorMessage } from '@/utils/utils'
+import { createRandomString, dataType, getErrorMsg, popUpErrorMessage } from '@/utils/utils'
 import { useNodeMsgMap } from './useNodeList'
 import { useGetPluginMsgIdMap } from './usePlugin'
 import useWriteDataCheckNParse from '@/composables/data/useWriteDataCheckNParse'
@@ -220,13 +220,11 @@ export default () => {
   }
 
   const { parseWriteData } = useWriteDataCheckNParse()
-  const submit = async () => {
+  const addTags = async () => {
     try {
-      isSubmitting.value = true
-      await tagFormRef.value.validate()
       const nodeName = route.params.node.toString()
       const groupName: string = route.params.group as string
-      const tags = formData.value.tagList.map(({ id, ...tagData }) => {
+      const tags: TagForm[] = formData.value.tagList.map(({ id, ...tagData }) => {
         const data = tagData
         const { type, value } = data
         if (value !== undefined && value !== null) {
@@ -238,18 +236,60 @@ export default () => {
 
         return data
       })
-
       await addTag({ tags, node: nodeName, group: groupName })
-      EmqxMessage.success(t('common.createSuccess'))
-      router.push({
-        name: 'SouthDriverGroupTag',
-      })
+      return Promise.resolve()
     } catch (error: any) {
-      console.error(error)
       const { data = {} } = error
       if (data.error !== 0 && data.index !== undefined) {
         handlePartialSuc(data.index, data.error)
       }
+      return Promise.reject(error)
+    }
+  }
+
+  const validateTagForm = async () => {
+    try {
+      await tagFormRef.value.validate()
+      return Promise.resolve()
+    } catch (error) {
+      let errorRows: string[] = []
+
+      const errorData = error || {}
+      if (dataType(errorData) === 'object') {
+        const keys = Object.keys(errorData)
+        keys.forEach((field: string) => {
+          const fieldSplit = field.split('.')
+          if (
+            fieldSplit &&
+            fieldSplit[1] &&
+            dataType(Number(fieldSplit[1])) === 'number' &&
+            !errorRows.includes(fieldSplit[1])
+          ) {
+            errorRows.push(fieldSplit[1])
+          }
+        })
+      }
+
+      errorRows = errorRows.sort()
+      const errorRow = errorRows[0]
+      if (errorRow) {
+        EmqxMessage.error(t('config.tableRowDataError', { rowNum: Number(errorRow) + 1 }))
+      }
+      return Promise.reject(error)
+    }
+  }
+
+  const submit = async () => {
+    try {
+      isSubmitting.value = true
+      await validateTagForm()
+      await addTags()
+      EmqxMessage.success(t('common.createSuccess'))
+      router.push({
+        name: 'SouthDriverGroupTag',
+      })
+    } catch (error) {
+      console.error(error)
     } finally {
       isSubmitting.value = false
     }
