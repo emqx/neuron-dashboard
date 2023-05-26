b10e81f3b1353f33831356c4a6791095d5c7c3f0
orange <gezuxia@163.com>
2023-05-26 16:02:16 +0800

    feat(template tag): support tag list

---
+++ src/composables/config/useTemplateTagList.ts
@@ -0,0 +1,165 @@
+import type { Ref } from 'vue'
+import { ref, computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useRoute } from 'vue-router'
+import { debounce } from 'lodash'
+import { EmqxMessage } from '@emqx/emqx-ui'
+import { queryTagList } from '@/api/template'
+import type { TagData, TagForm } from '@/types/config'
+import usePaging from '@/composables/usePaging'
+import { MessageBoxConfirm } from '@/utils/element'
+import { OmitArrayFields } from '@/utils/utils'
+
+interface TagDataInTable extends TagData {
+  checked: boolean
+}
+
+export default () => {
+  const route = useRoute()
+  const { t } = useI18n()
+
+  const tagList: Ref<Array<TagDataInTable>> = ref([])
+  const isListLoading: Ref<boolean> = ref(false)
+
+  const pageController = ref({
+    pageNum: 1,
+    pageSize: 50,
+    total: 0,
+  })
+
+  const queryKeyword = ref({
+    name: '',
+  })
+
+  const template = computed(() => route.params.template.toString())
+  const groupName = computed(() => route.params.group as string)
+
+  const allChecked = computed({
+    get() {
+      if (tagList.value.length === 0) {
+        return false
+      }
+      return tagList.value.every(({ checked }) => checked)
+    },
+    set(val: boolean) {
+      tagList.value.forEach((item) => {
+        item.checked = val
+      })
+    },
+  })
+
+  const tagCheckedList = computed(() => {
+    const checkedList: Array<TagDataInTable> = tagList.value.filter(({ checked }) => checked)
+    const newCheckedList: Array<TagData> = OmitArrayFields(checkedList, ['checked'])
+    return newCheckedList
+  })
+
+  const currentTag: Ref<TagForm> = ref({} as TagForm)
+
+  const { totalData, setTotalData, getAPageData } = usePaging()
+
+  const getAPageTagData = () => {
+    const { data, meta } = getAPageData(pageController.value)
+    tagList.value = data
+    pageController.value.total = meta.total
+  }
+
+  const getTagList = async () => {
+    isListLoading.value = true
+    const params = {
+      template: template.value,
+      group: groupName.value,
+      ...queryKeyword.value,
+    }
+    const data = await queryTagList(params)
+    setTotalData(data.map((item) => Object.assign(item, { checked: false })))
+    getAPageTagData()
+    isListLoading.value = false
+  }
+
+  // debounce
+  const dbGetTagList = debounce(() => {
+    getTagList()
+  }, 500)
+
+  const refreshTable = () => {
+    pageController.value.pageNum = 1
+    getTagList()
+  }
+
+  const handleSizeChange = (size: number) => {
+    pageController.value.pageSize = size
+    pageController.value.pageNum = 1
+    getAPageTagData()
+  }
+
+  // Delele tags
+  // const deleteTagList = async (list: Array<TagData>) => {
+  // const params = {
+  //   template: template.value,
+  //   group: groupName.value,
+  //   tags: list.map(({ name }) => name),
+  // }
+  // await deleteTag(params)
+  // EmqxMessage.success(t('common.operateSuccessfully'))
+  // refreshTable()
+  // }
+
+  const delTag = async (item: TagDataInTable) => {
+    console.log('delTag tag', item)
+    // await MessageBoxConfirm()
+
+    // const delList = OmitArrayFields([item], ['checked'])
+    // deleteTagList(delList)
+  }
+
+  const batchDeleteTag = async () => {
+    // await MessageBoxConfirm()
+    // deleteTagList(tagCheckedList.value)
+  }
+
+  const clearTag = async () => {
+    // await MessageBoxConfirm(t('common.confirmClear'))
+    // deleteTagList(totalData.value)
+  }
+
+  const goCreatePage = () => {
+    // TODO
+  }
+
+  // TODO: Edit tag
+  const editTag = (tag: TagDataInTable) => {
+    console.log('edit tag', tag)
+  }
+
+  getTagList()
+
+  return {
+    template,
+    groupName,
+
+    isListLoading,
+    tagList,
+    totalData,
+    tagCheckedList,
+    allChecked,
+
+    queryKeyword,
+
+    pageController,
+    getAPageTagData,
+    handleSizeChange,
+
+    getTagList,
+    dbGetTagList,
+    refreshTable,
+
+    delTag,
+    clearTag,
+    batchDeleteTag,
+
+    goCreatePage,
+    currentTag,
+    editTag,
+  }
+}
---
+++ src/views/config/template/Tag.vue
@@ -0,0 +1,133 @@
+<template>
+  <emqx-card v-emqx-loading="isListLoading" class="tag-page">
+    <h3 class="card-title">{{ $t('config.tagList') }}</h3>
+    <div class="card-bar-under-title common-flex">
+      <div class="bar-left">
+        <p class="driver-name">
+          <label>{{ $t('template.templateName') }}</label>
+          <span>{{ template }}</span>
+        </p>
+      </div>
+      <div class="btns common-flex">
+        <div class="btn-group">
+          <emqx-button size="small" type="primary" @click="goCreatePage">
+            {{ $t('common.create') }}
+          </emqx-button>
+          <emqx-button size="small" type="warning" :disabled="!tagList.length" @click="clearTag">{{
+            $t('common.clear')
+          }}</emqx-button>
+          <emqx-button size="small" type="danger" :disabled="!tagCheckedList.length" @click="batchDeleteTag">{{
+            $t('common.delete')
+          }}</emqx-button>
+          <KeywordSerachInput
+            v-model="queryKeyword.name"
+            class="search_input"
+            @input="dbGetTagList"
+            @clear="dbGetTagList"
+            @enter="dbGetTagList"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="table-container">
+      <emqx-table :data="tagList" :empty-text="$t('common.emptyData')">
+        <emqx-table-column :width="28">
+          <template #header>
+            <emqx-checkbox v-model="allChecked" />
+          </template>
+          <template #default="{ row }">
+            <emqx-checkbox v-model="row.checked" />
+          </template>
+        </emqx-table-column>
+        <emqx-table-column :label="$t('common.name')" prop="name"></emqx-table-column>
+        <emqx-table-column :label="$t('config.address')" prop="address"></emqx-table-column>
+
+        <emqx-table-column :label="$t('common.type')">
+          <template #default="{ row }">{{ findTagTypeLabelByValue(row.type) }}</template>
+        </emqx-table-column>
+        <emqx-table-column :label="$t('common.attribute')">
+          <template #default="{ row }">{{ getAttrStrByValue(row.attribute) }}</template>
+        </emqx-table-column>
+        <emqx-table-column :label="$t('config.decimal')">
+          <template #default="{ row }">{{ tagDecimalValue(row.decimal) }}</template>
+        </emqx-table-column>
+        <emqx-table-column :label="$t('config.precision')">
+          <template #default="{ row }">{{ tagPrecisionValue(row.type, row.precision) }}</template>
+        </emqx-table-column>
+        <emqx-table-column :label="$t('config.desc')" prop="description" />
+
+        <emqx-table-column align="left" :label="$t('common.oper')" width="140px">
+          <template #default="{ row }">
+            <AComWithDesc :content="$t('common.edit')">
+              <i class="el-icon-edit-outline" @click="editTag(row)" />
+            </AComWithDesc>
+            <AComWithDesc :content="$t('common.delete')">
+              <i class="iconfont icondelete" @click="delTag(row)" />
+            </AComWithDesc>
+          </template>
+        </emqx-table-column>
+      </emqx-table>
+    </div>
+    <emqx-pagination
+      v-if="pageController.total > 50"
+      layout="total, sizes, prev, pager, next, jumper"
+      v-model:current-page="pageController.pageNum"
+      :page-sizes="[50, 100, 200]"
+      :total="pageController.total"
+      :page-size="pageController.pageSize"
+      @current-change="getAPageTagData"
+      @size-change="handleSizeChange"
+    />
+  </emqx-card>
+</template>
+
+<script lang="ts" setup>
+import AComWithDesc from '@/components/AComWithDesc.vue'
+import KeywordSerachInput from '@/components/KeywordSearchInput.vue'
+import useTemplateTagList from '@/composables/config/useTemplateTagList'
+import {
+  useTagAttributeTypeSelect,
+  useTagTypeSelect,
+  useTagPrecision,
+  useTagDecimal,
+} from '@/composables/config/useAddTag'
+
+const {
+  template,
+
+  isListLoading,
+  tagList,
+  tagCheckedList,
+  allChecked,
+
+  pageController,
+  getAPageTagData,
+  handleSizeChange,
+
+  delTag,
+  batchDeleteTag,
+  clearTag,
+
+  queryKeyword,
+  dbGetTagList,
+
+  goCreatePage,
+  editTag,
+} = useTemplateTagList()
+const { tagPrecisionValue } = useTagPrecision()
+const { tagDecimalValue } = useTagDecimal()
+const { findLabelByValue: findTagTypeLabelByValue } = useTagTypeSelect()
+const { getAttrStrByValue } = useTagAttributeTypeSelect()
+</script>
+
+<style lang="scss">
+.tag-page {
+  .table-container {
+    margin-bottom: 24px;
+  }
+  .search_input {
+    margin-left: 10px;
+  }
+}
+</style>
