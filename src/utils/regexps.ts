5ad0669e8cd286788f4252ec443d448998e01587
orangegzx <zuxia.ge@emqx.io>
2023-06-13 15:35:59 +0800

    Refactor(regex): Regexes unified management

--- src/utils/regexps.ts
+++ src/utils/regexps.ts
@@ -1,1 +1,10 @@
 export const HEXADECIMAL_REGEX = /^0(x|X)[0-9A-Fa-f]+$/
+export const DECIMAL_POSITIVE_REGEX = /^[0-9]\d*$/
+
+export const FLOAT_REGEX = /^-?\d*\.?\d+(e-?\d+)?$/
+export const BIT_REGEX = /^[0-9a-f]+$/
+export const INT_REGEX = /^-?\d+$/
+export const BINARY_STRING_REGEX = /^(0|1)+$/
+
+export const UPPER_LOWERCASE_REGEX = /^[A-Z][a-z]+$/
+export const LOWERCASE_INITIAL_REGEX = /^[a-z]/
