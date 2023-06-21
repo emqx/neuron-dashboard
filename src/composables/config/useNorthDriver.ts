dff9dedcce066ecf22eb55e734d0ffe8c4477af7
orangegzx <zuxia.ge@emqx.io>
2023-06-21 15:31:17 +0800

    refactor(south, north): remove refresh after debug log

--- src/composables/config/useNorthDriver.ts
+++ src/composables/config/useNorthDriver.ts
@@ -138,7 +138,6 @@ export default (autoLoad = true, needRefreshStatus = false) => {
 
   const modifyNodeLogLevel = async (node: DriverItemInList) => {
     await modifyNodeLogLevelToDebug(node.name)
-    dbGetNorthDriverList()
   }
 
   if (autoLoad) {
